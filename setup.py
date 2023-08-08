c357f1c466b1bcfe066c00a1f7a789e305eceb86
Nick Wunder <nick.wunder@nrel.gov>
2023-08-08 11:59:42 -0600

    Add requires

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
 
 
-setup(name='hero',
-      version='0.0.1',
-      packages=['hero']
-      )
+setup(
+    name='hero',
+    version='0.0.1',
+    packages=['hero'],
+    install_requires=[
+        'boto3>=1.28.3',
+        'psycopg2-binary>=2.9.6',
+        'requests>=2.28.2'
+    ]
+)
