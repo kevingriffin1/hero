6da8c36764108930cb6d19ecd239f04cccdeae5f
Monte Lunacek <monte.lunacek@nrel.gov>
2023-09-19 05:52:16 -0600

    fixing build

--- hero/__init__.py
+++ hero/__init__.py
@@ -1,11 +1,7 @@
 """
 .. include:: ./documentation.md
 """
-from . import api
-from . import aws
-from . import auth
-from . import config
-from .hero_client import Hero
+
 
 from time import time
 
@@ -16,13 +12,21 @@ urllib3.disable_warnings(InsecureRequestWarning)
 import logging
 logging.basicConfig(level=logging.INFO)
 
-def timer(func):
-    """Prints the runtime of the decorated function"""
 
-    def wrap_func(*args, **kwargs):
-        tic = time()
-        result = func(*args, **kwargs)
-        log.into(f"Function {func.__name__!r} executed in {(time()-tic):.4f}s")
-        return result
+# from . import api
+# from . import aws
+# from . import auth
+# from . import config
+from .hero_client import Hero
 
-    return wrap_func
+# def timer(func):
+#     """Prints the runtime of the decorated function"""
+
+#     def wrap_func(*args, **kwargs):
+#         tic = time()
+#         result = func(*args, **kwargs)
+#         log.into(f"Function {func.__name__!r} executed in {(time()-tic):.4f}s")
+#         return result
+
+#     return wrap_func
+
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='hero',
     version='0.0.1',
-    packages=['hero'],
+    packages=['hero', 'hero.api', 'hero.aws', 'hero.auth', 'hero.config'],
     install_requires=[
         'boto3>=1.28.3',
         'psycopg2-binary>=2.9.6',
