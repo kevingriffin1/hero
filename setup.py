cc99fe7cae1a52938f28137af8c84f1c1ed4a1e6
Nick Wunder <nick.wunder@nrel.gov>
2023-11-15 17:34:14 -0700

    Hardcode version to avoid import from hero

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
-from hero import __version__
+# from hero import __version__
 
 setup(
     name='hero',
-    version=__version__,
+    version='0.1.4',
     packages=['hero', 'hero.api', 'hero.aws', 'hero.auth', 'hero.config'],
     entry_points={ "console_scripts": [
                         "hero_clear_queue=hero.bin.hero_clear_queue:main",
