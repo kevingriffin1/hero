317dd5d33191896bd24779479e84da1072a2415b
Nick Wunder <nick.wunder@nrel.gov>
2023-11-15 17:28:37 -0700

    Remove install requires

--- setup.py
+++ setup.py
@@ -5,11 +5,6 @@ setup(
     name='hero',
     version=__version__,
     packages=['hero', 'hero.api', 'hero.aws', 'hero.auth', 'hero.config'],
-    install_requires=[
-        'botocore>=1.31.76',
-        'boto3>=1.28.3',
-        'requests>=2.28.2'
-    ],
     entry_points={ "console_scripts": [
                         "hero_clear_queue=hero.bin.hero_clear_queue:main",
             ]},
