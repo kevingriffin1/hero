670cd8cc008c855834d2f53fab34c8bcc44e1dbd
Monte Lunacek <monte.lunacek@nrel.gov>
2024-02-17 09:22:38 -0700

    updating setup.py with setup requires

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
 
+requires = [
+    "boto3>=1.28.3",
+    "psycopg2-binary>=2.9.6",
+    "requests>=2.28.2",
+    "pyyaml>=6.0",
+    "pytest>=7.4.0",
+    "numpy",
+]
+
 
 setup(
     name="hero",
     version="0.0.1",
     packages=["hero", "hero.api", "hero.aws", "hero.auth", "hero.config"],
-    install_requires=["boto3>=1.28.3", "psycopg2-binary>=2.9.6", "requests>=2.28.2"],
+    install_requires=requires,
+    setup_requires=requires,
     entry_points={
         "console_scripts": [
             "hero_clear_queue=hero.bin.hero_clear_queue:main",
