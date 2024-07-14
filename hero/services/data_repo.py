25d8b4edb11b89d9b21a68699f0da902bc42af1e
Monte Lunacek <monte.lunacek@nrel.gov>
2024-07-14 16:05:37 -0600

    potential functionality

--- hero/services/data_repo.py
+++ hero/services/data_repo.py
@@ -1,16 +1,18 @@
 import json
-
+import os
 from ..url_map import URL_MAP
 from ..lib import ServiceBase, decorate_all, log_errors, get_conf_from_collection
+from requests.exceptions import HTTPError
 
-@decorate_all(log_errors)
+
+# @decorate_all(log_errors)
 class DataRepoService(ServiceBase):
     def _configure(self):
-        '''
+        """
         Sets the API, adds data_repo id and required scope
-        '''
-        self.client.add_scope('data-repo/user')
-        self.base_url = get_conf_from_collection(URL_MAP, 'HERO_DATA_REPO_API_URL')
+        """
+        self.client.add_scope("data-repo/user")
+        self.base_url = get_conf_from_collection(URL_MAP, "HERO_DATA_REPO_API_URL")
 
     # export async function getProjects(setData, user, dataRepoId) {
     #     const requestHeaders = createRequestHeaders(user);
@@ -21,10 +23,9 @@ class DataRepoService(ServiceBase):
     # }
     def get_projects(self, datarepo_id):
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/projects'
-        response = self.api.request('GET', url, headers=headers)
+        url = f"{self.base_url}/{datarepo_id}/projects"
+        response = self.api.request("GET", url, headers=headers)
         return response.json()
-
 
     # export async function getProjectDatasets(setData, user, dataRepoId, projectId) {
     #     const requestHeaders = createRequestHeaders(user);
@@ -36,10 +37,9 @@ class DataRepoService(ServiceBase):
     #
     def get_project_datasets(self, datarepo_id, project_id):
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/project/{project_id}/datasets'
-        response = self.api.request('GET', url, headers=headers)
+        url = f"{self.base_url}/{datarepo_id}/project/{project_id}/datasets"
+        response = self.api.request("GET", url, headers=headers)
         return response.json()
-
 
     # export async function getProject(setData, user, dataRepoId, projectId) {
     #     const requestHeaders = createRequestHeaders(user);
@@ -50,15 +50,18 @@ class DataRepoService(ServiceBase):
     # }
     def get_project(self, datarepo_id, project_id):
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/project/{project_id}'
-        response = self.api.request('GET', url, headers=headers)
+        url = f"{self.base_url}/{datarepo_id}/project/{project_id}"
+        response = self.api.request("GET", url, headers=headers)
         return response.json()
-    
-    def read_project_by_name(self, datarepo_id, metatype, name):
+
+    def read_project_by_name(self, datarepo_id, name, metatype="Project"):
+        """This returns a single project even if there are many with the same name
+        and returns an 404 error if there are no projects, nice!
+        """
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/project/metatype/{metatype}'
-        params = f'name={name}'
-        response = self.api.request('GET', url, headers=headers, params=params)
+        url = f"{self.base_url}/{datarepo_id}/project/metatype/{metatype}"
+        params = f"name={name}"
+        response = self.api.request("GET", url, headers=headers, params=params)
         return response.json()
 
     # export async function deleteProject(user, dataRepoId, projectId) {
@@ -70,9 +73,10 @@ class DataRepoService(ServiceBase):
     # }
     def delete_project(self, datarepo_id, project_id):
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/project/{project_id}'
-        response = self.api.request('DELETE', url, headers=headers)
-        return response.json()
+        url = f"{self.base_url}/{datarepo_id}/project/{project_id}"
+        response = self.api.request("DELETE", url, headers=headers)
+        return response
+        # return response.json()
 
     # export async function addProject(user, dataRepoId, attributes) {
     #     const requestHeaders = createRequestHeaders(user);
@@ -83,11 +87,13 @@ class DataRepoService(ServiceBase):
     #     });
     #     return response.data;
     # }
-    def add_project(self, datarepo_id, attributes):
+    def add_project(self, datarepo_id, project_name, metatype="Project"):
+        attributes = {"name": project_name, "metatype": metatype}
+
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/project'
+        url = f"{self.base_url}/{datarepo_id}/project"
         data = json.dumps(attributes)
-        response = self.api.request('POST', url, headers=headers, data=data)
+        response = self.api.request("POST", url, headers=headers, data=data)
         return response.json()
 
     # export async function updateProject(user, dataRepoId, projectId, attributes) {
@@ -101,9 +107,9 @@ class DataRepoService(ServiceBase):
     # }
     def update_project(self, datarepo_id, project_id, attributes):
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/project/{project_id}'
+        url = f"{self.base_url}/{datarepo_id}/project/{project_id}"
         data = json.dumps(attributes)
-        response = self.api.request('PUT', url, headers=headers, data=data)
+        response = self.api.request("PUT", url, headers=headers, data=data)
         return response.json()
 
     # export async function getDatasets(setData, user, dataRepoId) {
@@ -115,8 +121,8 @@ class DataRepoService(ServiceBase):
     # }
     def get_datasets(self, datarepo_id):
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/datasets'
-        response = self.api.request('GET', url, headers=headers)
+        url = f"{self.base_url}/{datarepo_id}/datasets"
+        response = self.api.request("GET", url, headers=headers)
         return response.json()
 
     # export async function getDataset(setData, user, dataRepoId, datasetId) {
@@ -128,15 +134,15 @@ class DataRepoService(ServiceBase):
     # }
     def get_dataset(self, datarepo_id, dataset_id):
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/dataset/{dataset_id}'
-        response = self.api.request('GET', url, headers=headers)
+        url = f"{self.base_url}/{datarepo_id}/dataset/{dataset_id}"
+        response = self.api.request("GET", url, headers=headers)
         return response.json()
 
-    def read_dataset_by_name(self, datarepo_id, metatype, name):
+    def read_dataset_by_name(self, datarepo_id, name, metatype="Dataset"):
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/dataset/metatype/{metatype}'
-        params = f'name={name}'
-        response = self.api.request('GET', url, headers=headers, params=params)
+        url = f"{self.base_url}/{datarepo_id}/dataset/metatype/{metatype}"
+        params = f"name={name}"
+        response = self.api.request("GET", url, headers=headers, params=params)
         response.raise_for_status()
         return response.json()
 
@@ -149,8 +155,8 @@ class DataRepoService(ServiceBase):
     # }
     def get_dataset_files(self, datarepo_id, dataset_id):
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/dataset/{dataset_id}/files'
-        response = self.api.request('GET', url, headers=headers)
+        url = f"{self.base_url}/{datarepo_id}/dataset/{dataset_id}/files"
+        response = self.api.request("GET", url, headers=headers)
         return response.json()
 
     # export async function deleteDataset(user, dataRepoId, datasetId) {
@@ -162,9 +168,9 @@ class DataRepoService(ServiceBase):
     # }
     def delete_dataset(self, datarepo_id, dataset_id):
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/dataset/{dataset_id}'
-        response = self.api.request('DELETE', url, headers=headers)
-        return response.json()
+        url = f"{self.base_url}/{datarepo_id}/dataset/{dataset_id}"
+        response = self.api.request("DELETE", url, headers=headers)
+        return response
 
     # export async function addDataset(user, dataRepoId, attributes) {
     #     const requestHeaders = createRequestHeaders(user);
@@ -175,11 +181,19 @@ class DataRepoService(ServiceBase):
     #     });
     #     return response.data;
     # }
-    def add_dataset(self, datarepo_id, attributes):
+    def add_dataset(
+        self, datarepo_id, project_id, dataset_name, metatype="Dataset", metadata={}
+    ):
+        attributes = {
+            "projectId": project_id,
+            "name": dataset_name,
+            "metatype": metatype,
+            "metadata": metadata,
+        }
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/dataset'
+        url = f"{self.base_url}/{datarepo_id}/dataset"
         data = json.dumps(attributes)
-        response = self.api.request('POST', url, headers=headers, data=data)
+        response = self.api.request("POST", url, headers=headers, data=data)
         return response.json()
 
     # export async function updateDataset(user, dataRepoId, datasetId, attributes) {
@@ -193,9 +207,9 @@ class DataRepoService(ServiceBase):
     # }
     def update_dataset(self, datarepo_id, dataset_id, attributes):
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/dataset/{dataset_id}'
+        url = f"{self.base_url}/{datarepo_id}/dataset/{dataset_id}"
         data = json.dumps(attributes)
-        response = self.api.request('PUT', url, headers=headers, data=data)
+        response = self.api.request("PUT", url, headers=headers, data=data)
         return response.json()
 
     # export async function getFiles(setData, user, dataRepoId) {
@@ -207,8 +221,8 @@ class DataRepoService(ServiceBase):
     # }
     def get_files(self, datarepo_id):
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/files'
-        response = self.api.request('GET', url, headers=headers)
+        url = f"{self.base_url}/{datarepo_id}/files"
+        response = self.api.request("GET", url, headers=headers)
         return response.json()
 
     # export async function getFile(setData, user, dataRepoId, fileId) {
@@ -220,15 +234,15 @@ class DataRepoService(ServiceBase):
     # }
     def get_file(self, datarepo_id, file_id):
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/file/{file_id}'
-        response = self.api.request('GET', url, headers=headers)
+        url = f"{self.base_url}/{datarepo_id}/file/{file_id}"
+        response = self.api.request("GET", url, headers=headers)
         return response.json()
 
-    def read_file_by_name(self, datarepo_id, metatype, name):
+    def read_file_by_name(self, datarepo_id, name, metatype="File"):
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/file/metatype/{metatype}'
-        params = f'name={name}'
-        response = self.api.request('GET', url, headers=headers, params=params)
+        url = f"{self.base_url}/{datarepo_id}/file/metatype/{metatype}"
+        params = f"name={name}"
+        response = self.api.request("GET", url, headers=headers, params=params)
         return response.json()
 
     # /**
@@ -244,8 +258,8 @@ class DataRepoService(ServiceBase):
     # }
     def get_file_download_url(self, datarepo_id, file_id):
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/files/download/{file_id}'
-        response = self.api.request('GET', url, headers=headers)
+        url = f"{self.base_url}/{datarepo_id}/files/download/{file_id}"
+        response = self.api.request("GET", url, headers=headers)
         return response.json()
 
     # /**
@@ -256,7 +270,7 @@ class DataRepoService(ServiceBase):
     #     setData(response.data);
     # }
     def download_file(self, url):
-        response = self.api.request('GET', url)
+        response = self.api.request("GET", url)
         return response.json()
 
     # export async function deleteFile(user, dataRepoId, fileId) {
@@ -268,9 +282,9 @@ class DataRepoService(ServiceBase):
     # }
     def delete_file(self, datarepo_id, file_id):
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/file/{file_id}'
-        response = self.api.request('DELETE', url, headers=headers)
-        return response.json()
+        url = f"{self.base_url}/{datarepo_id}/file/{file_id}"
+        response = self.api.request("DELETE", url, headers=headers)
+        return response
 
     # export async function addFile(user, dataRepoId, attributes) {
     #     const requestHeaders = createRequestHeaders(user);
@@ -281,12 +295,28 @@ class DataRepoService(ServiceBase):
     #         });
     #     return response.data;
     # }
-    def add_file(self, datarepo_id, attributes):
+    def add_file(self, datarepo_id, dataset_id, filename, metatype="File", metadata={}):
+        attributes = {
+            "name": filename,
+            "datasetId": dataset_id,
+            "metatype": metatype,
+            "metadata": metadata,
+        }
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/file'
+        url = f"{self.base_url}/{datarepo_id}/file"
         data = json.dumps(attributes)
-        response = self.api.request('POST', url, headers=headers, data=data)
+        response = self.api.request("POST", url, headers=headers, data=data)
         return response.json()
+
+    def upload_file(self, signed_url, file_path):
+
+        # Read the file data
+        with open(file_path, "rb") as file:
+            file_data = file.read()
+
+        # Make a PUT request to the signed URL
+        response = self.api.put(signed_url, data=file_data)
+        return response
 
     # export async function updateFile(user, dataRepoId, fileId, attributes) {
     #     const requestHeaders = createRequestHeaders(user);
@@ -299,9 +329,9 @@ class DataRepoService(ServiceBase):
     # }
     def update_file(self, datarepo_id, file_id, attributes):
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/file/{file_id}'
+        url = f"{self.base_url}/{datarepo_id}/file/{file_id}"
         data = json.dumps(attributes)
-        response = self.api.request('PUT', url, headers=headers, data=data)
+        response = self.api.request("PUT", url, headers=headers, data=data)
         return response.json()
 
     # /**
@@ -319,9 +349,63 @@ class DataRepoService(ServiceBase):
     # }
     def get_file_upload_url(self, datarepo_id, file_id):
         headers = self.get_headers(self.client.get_token())
-        url = f'{self.base_url}/{datarepo_id}/files/upload/{file_id}'
-        response = self.api.request('GET', url, headers=headers)
+        url = f"{self.base_url}/{datarepo_id}/files/upload/{file_id}"
+        response = self.api.request("GET", url, headers=headers)
         return response.json()
 
-    def hello(self):
-        print('hello')
+    # what do you think about these files?
+    # ====================================
+    def get_or_create_project(self, DATA_REPO_ID, project_name):
+        try:
+            project = self.read_project_by_name(DATA_REPO_ID, project_name)
+            return project
+        except HTTPError as err:
+            project = self.add_project(DATA_REPO_ID, project_name)
+            return project
+
+    def get_or_create_dataset(self, DATA_REPO_ID, project_id, dataset_name):
+        try:
+            dataset = self.read_dataset_by_name(DATA_REPO_ID, dataset_name)
+            return dataset
+        except HTTPError as err:
+            dataset = self.add_dataset(DATA_REPO_ID, project_id, dataset_name)
+            return dataset
+
+    def add_file_if_not_exists(self, DATA_REPO_ID, dataset_id, filename):
+        assert os.path.exists(filename)
+        base_name = os.path.basename(filename)
+        try:
+            fileobj = self.read_file_by_name(DATA_REPO_ID, base_name)
+            return fileobj
+        except HTTPError as err:
+            fileobj = self.add_file(DATA_REPO_ID, dataset_id, base_name)
+            url = self.get_file_upload_url(DATA_REPO_ID, fileobj["id"])
+            self.upload_file(url["url"], filename)
+            fileobj = self.read_file_by_name(DATA_REPO_ID, base_name)
+            return fileobj
+
+    def add_or_replace_file(self, DATA_REPO_ID, dataset_id, filename):
+        assert os.path.exists(filename)
+        base_name = os.path.basename(filename)
+        try:
+            fileobj = self.read_file_by_name(DATA_REPO_ID, base_name)
+        except HTTPError as err:
+            fileobj = self.add_file(DATA_REPO_ID, dataset_id, base_name)
+        finally:
+            url = self.get_file_upload_url(DATA_REPO_ID, fileobj["id"])
+            self.upload_file(url["url"], filename)
+            fileobj = self.read_file_by_name(DATA_REPO_ID, base_name)
+            return fileobj
+
+    def download_file_as(self, DATA_REPO_ID, filename, new_filename):
+        fileobj = self.read_file_by_name(DATA_REPO_ID, filename)
+        url = self.get_file_download_url(DATA_REPO_ID, fileobj["id"])
+
+        with self.api.get(url["url"], stream=True) as r:
+            r.raise_for_status()
+            try:
+                with open(new_filename, "wb") as f:
+                    for chunk in r.iter_content(chunk_size=8192):
+                        f.write(chunk)
+            except Exception as e:
+                print(f"File write to disk failed with error: {str(e)}")
