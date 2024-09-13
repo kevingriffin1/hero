726a0bdef489978dfaad09deb0d8a4b3802a3ffd
Smith, Joseph <Joseph.Smith@nrel.gov>
2024-09-12 21:10:06 -0400

    Adds remaining permissions methods and tests

--- hero/services/auth.py
+++ hero/services/auth.py
@@ -16,6 +16,70 @@ class AuthService(ServiceBase):
         self.client.add_scope("hero-auth/user")
         self.base_url = get_conf_from_collection(URL_MAP, "HERO_AUTH_API_URL")
 
+    def create_permission(self, appType=None, appId=None, principalType=None, principalId=None, resourceType=None, resourceId=None, permissionSet=None):
+        """
+        Creates a permission for the data repo
+        """
+
+        if appType is None:
+            raise MissingRequiredAttribute('Missing required attribute: "appType"')
+        if appId is None:
+            raise MissingRequiredAttribute('Missing required attribute: "appId"')
+        if principalType is None:
+            raise MissingRequiredAttribute('Missing required attribute: "principalType"')
+        if principalId is None:
+            raise MissingRequiredAttribute('Missing required attribute: "principalId"')
+        if resourceType is None:
+            raise MissingRequiredAttribute('Missing required attribute: "resourceType"')
+        if resourceId is None:
+            raise MissingRequiredAttribute('Missing required attribute: "resourceId"')
+        if permissionSet is None:
+            raise MissingRequiredAttribute('Missing required attribute: "permissionSet"')
+
+        attributes = {
+            "permissionSet": permissionSet
+        }
+
+        headers = self.get_headers(self.client.get_token())
+        url = f"{self.base_url}/permission/{appType}/{appId}/{principalType}/{principalId}/{resourceType}/{resourceId}"
+        data = json.dumps(attributes)
+
+        try:
+            response = self.api.request("POST", url, headers=headers, data=data)
+            return response.json()
+        except JSONDecodeError:
+            raise HEROAPIResponseException()
+        except HTTPError as e:
+            raise e
+
+    def read_permission(self, appType=None, appId=None, principalType=None, principalId=None, resourceType=None, resourceId=None):
+        """
+        Reads a permission for the data repo
+        """
+
+        if appType is None:
+            raise MissingRequiredAttribute('Missing required attribute: "appType"')
+        if appId is None:
+            raise MissingRequiredAttribute('Missing required attribute: "appId"')
+        if principalType is None:
+            raise MissingRequiredAttribute('Missing required attribute: "principalType"')
+        if principalId is None:
+            raise MissingRequiredAttribute('Missing required attribute: "principalId"')
+        if resourceType is None:
+            raise MissingRequiredAttribute('Missing required attribute: "resourceType"')
+        if resourceId is None:
+            raise MissingRequiredAttribute('Missing required attribute: "resourceId"')
+
+        headers = self.get_headers(self.client.get_token())
+        url = f"{self.base_url}/permission/{appType}/{appId}/{principalType}/{principalId}/{resourceType}/{resourceId}"
+
+        try:
+            response = self.api.request("GET", url, headers=headers)
+            return response.json()
+        except JSONDecodeError:
+            raise HEROAPIResponseException()
+        except HTTPError as e:
+            raise e
 
     def read_permissions(self, appType=None, appId=None):
         """
@@ -32,6 +96,70 @@ class AuthService(ServiceBase):
 
         try:
             response = self.api.request("GET", url, headers=headers)
+            return response.json()
+        except JSONDecodeError:
+            raise HEROAPIResponseException()
+        except HTTPError as e:
+            raise e
+
+    def update_permission(self, appType=None, appId=None, principalType=None, principalId=None, resourceType=None, resourceId=None, permissionSet=None):
+        """
+        Updates a permission for the data repo
+        """
+
+        if appType is None:
+            raise MissingRequiredAttribute('Missing required attribute: "appType"')
+        if appId is None:
+            raise MissingRequiredAttribute('Missing required attribute: "appId"')
+        if principalType is None:
+            raise MissingRequiredAttribute('Missing required attribute: "principalType"')
+        if principalId is None:
+            raise MissingRequiredAttribute('Missing required attribute: "principalId"')
+        if resourceType is None:
+            raise MissingRequiredAttribute('Missing required attribute: "resourceType"')
+        if resourceId is None:
+            raise MissingRequiredAttribute('Missing required attribute: "resourceId"')
+        if permissionSet is None:
+            raise MissingRequiredAttribute('Missing required attribute: "permissionSet"')
+
+        attributes = {
+            "permissionSet": permissionSet
+        }
+        headers = self.get_headers(self.client.get_token())
+        url = f"{self.base_url}/permission/{appType}/{appId}/{principalType}/{principalId}/{resourceType}/{resourceId}"
+        data = json.dumps(attributes)
+
+        try:
+            response = self.api.request("PUT", url, headers=headers, data=data)
+            return response.json()
+        except JSONDecodeError:
+            raise HEROAPIResponseException()
+        except HTTPError as e:
+            raise e
+
+    def delete_permission(self, appType=None, appId=None, principalType=None, principalId=None, resourceType=None, resourceId=None):
+        """
+        Deletes a permission for the data repo
+        """
+
+        if appType is None:
+            raise MissingRequiredAttribute('Missing required attribute: "appType"')
+        if appId is None:
+            raise MissingRequiredAttribute('Missing required attribute: "appId"')
+        if principalType is None:
+            raise MissingRequiredAttribute('Missing required attribute: "principalType"')
+        if principalId is None:
+            raise MissingRequiredAttribute('Missing required attribute: "principalId"')
+        if resourceType is None:
+            raise MissingRequiredAttribute('Missing required attribute: "resourceType"')
+        if resourceId is None:
+            raise MissingRequiredAttribute('Missing required attribute: "resourceId"')
+
+        headers = self.get_headers(self.client.get_token())
+        url = f"{self.base_url}/permission/{appType}/{appId}/{principalType}/{principalId}/{resourceType}/{resourceId}"
+
+        try:
+            response = self.api.request("DELETE", url, headers=headers)
             return response.json()
         except JSONDecodeError:
             raise HEROAPIResponseException()
--- test/test_auth.py
+++ test/test_auth.py
@@ -4,8 +4,56 @@ import os
 import json
 import time
 
-def test_read_permissions():
+def test_create_permission():
+        hero_client = hero.HeroClient()
+        auth = hero_client.Auth()
+        hero_client.authenticate()
 
+        appType = "data-hub"
+        appId = "dev-hero-test-framework"
+        principalType = "user"
+        principalId = "python-app-test-user"
+        resourceType = "data-repo"
+        resourceId = "dev-hero-test-framework"
+        permissionSet = [
+            "READ_PROJECT",
+            "READ_DATASET",
+            "READ_FILE"
+        ]
+
+        res = auth.create_permission(appType=appType, appId=appId, principalType=principalType, principalId=principalId, resourceType=resourceType, resourceId=resourceId, permissionSet=permissionSet)
+        assert type(res) is dict
+        print('res here pls!!!!!!!!!!!')
+        print(res)
+        assert res["permissionSet"] == permissionSet
+
+def test_read_permission():
+    hero_client = hero.HeroClient()
+    auth = hero_client.Auth()
+    hero_client.authenticate()
+
+    appType = "data-hub"
+    appId = "dev-hero-test-framework"
+    principalType = "user"
+    principalId = "python-app-test-user"
+    resourceType = "data-repo"
+    resourceId = "dev-hero-test-framework"
+
+    res = auth.read_permission(appType=appType, appId=appId, principalType=principalType, principalId=principalId, resourceType=resourceType, resourceId=resourceId)
+    assert type(res) is dict
+    assert res["appType"] == appType
+    assert res["appId"] == appId
+    assert res["principalType"] == principalType
+    assert res["principalId"] == principalId
+    assert res["resourceType"] == resourceType
+    assert res["resourceId"] == resourceId
+    assert res["permissionSet"] == [
+        "READ_PROJECT",
+        "READ_DATASET",
+        "READ_FILE"
+    ]
+
+def test_read_permissions():
     hero_client = hero.HeroClient()
     auth = hero_client.Auth()
     hero_client.authenticate()
@@ -15,3 +63,48 @@ def test_read_permissions():
 
     permissions = auth.read_permissions(appType=appType, appId=appId)
     assert type(permissions) is list
+
+def test_update_permission():
+    hero_client = hero.HeroClient()
+    auth = hero_client.Auth()
+    hero_client.authenticate()
+
+    appType = "data-hub"
+    appId = "dev-hero-test-framework"
+    principalType = "user"
+    principalId = "python-app-test-user"
+    resourceType = "data-repo"
+    resourceId = "dev-hero-test-framework"
+    permissionSet = [
+        "READ_PROJECT",
+        "READ_DATASET",
+        "READ_FILE",
+        "WRITE_PROJECT",
+        "WRITE_DATASET",
+        "WRITE_FILE"
+    ]
+
+    res = auth.update_permission(appType=appType, appId=appId, principalType=principalType, principalId=principalId, resourceType=resourceType, resourceId=resourceId, permissionSet=permissionSet)
+    assert type(res) is dict
+    assert res["permissionSet"] == permissionSet
+
+def test_delete_permission():
+    hero_client = hero.HeroClient()
+    auth = hero_client.Auth()
+    hero_client.authenticate()
+
+    appType = "data-hub"
+    appId = "dev-hero-test-framework"
+    principalType = "user"
+    principalId = "python-app-test-user"
+    resourceType = "data-repo"
+    resourceId = "dev-hero-test-framework"
+
+    res = auth.delete_permission(appType=appType, appId=appId, principalType=principalType, principalId=principalId, resourceType=resourceType, resourceId=resourceId)
+    assert type(res) is dict
+    assert res["appType"] == appType
+    assert res["appId"] == appId
+    assert res["principalType"] == principalType
+    assert res["principalId"] == principalId
+    assert res["resourceType"] == resourceType
+    assert res["resourceId"] == resourceId
