3eca1687f4bd4aef376208103dec7dc11cde0666
Smith, Joseph <Joseph.Smith@nrel.gov>
2024-09-24 09:21:33 -0400

    Adds comments to the test runner file

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,10 @@
-
+# Description: Run the tests under the Hero Test Framework project moniker
 export HERO_ENV="dev"
 export HERO_PROJECT="hero-test-framework"
+# Use these credentials for testing purposes only, do not use them in production
+# These are the test credentials for the Hero Test Framework project, if they
+# are missing permsissions or are founnd to have other access issues that affect
+# the tests, please contact the project owner.
 export HERO_CLIENT_ID="306m74pgmccj9qd1ccb876fdgk"
 export HERO_CLIENT_SECRET="u21676f1mrcqmhh5m723l1og4urjbjjg0oir37d4k2j7frpa9i0"
 
@@ -12,6 +16,7 @@ pytest test
 # Or just run a single suite/collection
 # pytest -s -v test/test_auth.py
 
+# Unset the environment variables
 unset HERO_ENV
 unset HERO_PROJECT
 unset HERO_CLIENT_ID
