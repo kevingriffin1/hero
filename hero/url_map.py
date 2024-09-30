21a043a3c1ef725e07fe5633e93316ca3ce317b5
Smith, Joseph <Joseph.Smith@nrel.gov>
2024-09-30 09:30:12 -0400

    Adds commented localhost hero auth URL

--- hero/url_map.py
+++ hero/url_map.py
@@ -5,6 +5,7 @@ URL_MAP = {
         # "HERO_DATA_REPO_API_URL": "http://localhost:8002/data-repo/api/v1",
         "HERO_DATA_REPO_API_URL": "https://dev-hero.nrel.gov/data-repo/api/v1",
         "HERO_SEARCH_API_URL": "https://dev-hero.nrel.gov/search/api/v1",
+        # "HERO_AUTH_API_URL": "http://localhost:8001/auth/api/v1",
         "HERO_AUTH_API_URL": "https://dev-hero.nrel.gov/auth/api/v1",
         "HERO_COGNITO_API_URL": "https://dev-nrel-research.auth.us-west-2.amazoncognito.com/oauth2/token",
         "HERO_ML_MODEL_REGISTRY_URL": "https://3akg7hmv58.execute-api.us-west-2.amazonaws.com/dev/m3s/api/v1",
