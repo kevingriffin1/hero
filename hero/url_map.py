ef620f60ec4796fe38d0fa8dc6375e4df0f8d3f4
Smith, Joseph <Joseph.Smith@nrel.gov>
2024-10-15 15:28:04 -0400

    Adds ml-model-registry dev endpoint to url map (HPY-45)

--- hero/url_map.py
+++ hero/url_map.py
@@ -7,7 +7,7 @@ URL_MAP = {
         "HERO_SEARCH_API_URL": "https://dev-hero.nrel.gov/search/api/v1",
         "HERO_AUTH_API_URL": "https://dev-hero.nrel.gov/auth/api/v1",
         "HERO_COGNITO_API_URL": "https://dev-nrel-research.auth.us-west-2.amazoncognito.com/oauth2/token",
-        "HERO_ML_MODEL_REGISTRY_URL": "https://3akg7hmv58.execute-api.us-west-2.amazonaws.com/dev/m3s/api/v1",
+        "HERO_ML_MODEL_REGISTRY_URL": "https://dev-hero.nrel.gov/ml-model-registry/api/v1",
     },
     "stage": {
         "HERO_TASK_ENGINE_API_URL": "https://stage-hero.nrel.gov/task-engine/api/v1",
@@ -15,7 +15,7 @@ URL_MAP = {
         "HERO_SEARCH_API_URL": "https://stage-hero.nrel.gov/search/api/v1",
         "HERO_AUTH_API_URL": "https://stage-hero.nrel.gov/auth/api/v1",
         "HERO_COGNITO_API_URL": "https://stage-nrel-research.auth.us-west-2.amazoncognito.com/oauth2/token",
-        "HERO_ML_MODEL_REGISTRY_URL": "https://3akg7hmv58.execute-api.us-west-2.amazonaws.com/dev/m3s/api/v1",
+        "HERO_ML_MODEL_REGISTRY_URL": "",
     },
     "production": {
         "HERO_TASK_ENGINE_API_URL": "https://hero.nrel.gov/task-engine/api/v1",
@@ -23,6 +23,6 @@ URL_MAP = {
         "HERO_SEARCH_API_URL": "https://hero.nrel.gov/search/api/v1",
         "HERO_AUTH_API_URL": "https://hero.nrel.gov/auth/api/v1",
         "HERO_COGNITO_API_URL": "https://nrel-research.auth.us-west-2.amazoncognito.com/oauth2/token",
-        "HERO_ML_MODEL_REGISTRY_URL": "https://3akg7hmv58.execute-api.us-west-2.amazonaws.com/dev/m3s/api/v1",
+        "HERO_ML_MODEL_REGISTRY_URL": "",
     },
 }
