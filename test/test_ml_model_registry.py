5081c6f32bee5ec5a25e1c874891cb8d0e80f6e0
Smith, Joseph <Joseph.Smith@nrel.gov>
2024-11-21 09:10:38 -0500

    Updates ml-model-registry to match new API structure
    Closes HPY-62, HPY-64, HPY-66, and HPY-68

--- test/test_ml_model_registry.py
+++ test/test_ml_model_registry.py
@@ -12,12 +12,31 @@ TESTABLE_RUN_ID = "c863ba10af2143129a31f2ceecf9d66d"
 #     assert uri == 'https://3akg7hmv58.execute-api.us-west-2.amazonaws.com/dev/m3s/api/v1/proxy/sandbox-m3s'
 
 
+def test_list_experiments():
+    hero_client = hero.HeroClient()
+    model_registry = hero_client.MLModelRegistry(TESTABLE_PROJECT_ID)
+    hero_client.authenticate()
+    res = model_registry.list_experiments()
+    assert isinstance(res, list)
+    assert len(res) > 0
+
+
 def test_read_experiment():
     hero_client = hero.HeroClient()
     model_registry = hero_client.MLModelRegistry(TESTABLE_PROJECT_ID)
     hero_client.authenticate()
     res = model_registry.read_experiment(TESTABLE_EXPERIMENT_ID)
+    assert isinstance(res, dict)
     assert res["id"] == TESTABLE_EXPERIMENT_ID
+
+
+def test_list_runs():
+    hero_client = hero.HeroClient()
+    model_registry = hero_client.MLModelRegistry(TESTABLE_PROJECT_ID)
+    hero_client.authenticate()
+    res = model_registry.list_runs(TESTABLE_EXPERIMENT_ID)
+    assert isinstance(res, list)
+    assert len(res) > 0
 
 
 def test_read_run():
@@ -25,6 +44,7 @@ def test_read_run():
     model_registry = hero_client.MLModelRegistry(TESTABLE_PROJECT_ID)
     hero_client.authenticate()
     res = model_registry.read_run(TESTABLE_RUN_ID)
+    assert isinstance(res, dict)
     assert res["id"] == TESTABLE_RUN_ID
 
 
@@ -33,4 +53,5 @@ def test_list_artifacts():
     model_registry = hero_client.MLModelRegistry(TESTABLE_PROJECT_ID)
     hero_client.authenticate()
     res = model_registry.list_artifacts(TESTABLE_RUN_ID)
-    assert len(res) == 10
+    assert isinstance(res, list)
+    assert len(res) > 0
