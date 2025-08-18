ac436f54ebcb2fa1bb622f0c81b647f0170220ed
Gunn Jungpaibul <kjungpai@nrel.gov>
2025-08-18 16:53:20 -0600

    Fix Task Engine failing tests

--- test/test_task_engine.py
+++ test/test_task_engine.py
@@ -1,7 +1,5 @@
 import hero
 
-APP_ID = "hero-test-framework"
-
 # previously created queue
 TESTABLE_QUEUE_ID = "4427b372-67a5-46c8-9e82-52fa979553d7"
 
@@ -9,40 +7,27 @@ TESTABLE_QUEUE_ID = "4427b372-67a5-46c8-9e82-52fa979553d7"
 def test_read_queues():
     hero_client = hero.HeroClient()
     task_engine = hero_client.TaskEngine()
-    queues = task_engine.read_queues(APP_ID)
+    queues = task_engine.read_queues()
     assert queues is not None
 
 
-def test_create_queue():
+def test_read_queue():
+    hero_client = hero.HeroClient()
+    task_engine = hero_client.TaskEngine()
+    queue = task_engine.read_queue(TESTABLE_QUEUE_ID)
+    assert queue["id"] == TESTABLE_QUEUE_ID
+
+
+def test_add_and_delete_queue():
     hero_client = hero.HeroClient()
     task_engine = hero_client.TaskEngine()
 
-    queue_attributes = {"name": "example_queue", "description": "example_description"}
-    queue = task_engine.create_queue(APP_ID, queue_attributes)
+    # add a queue
+    metadata = {"description": "example_description"}
+    queue = task_engine.add_queue(name="example_queue", metadata=metadata)
+    tmp_queue_id = queue["id"]
     assert queue["name"] == "example_queue"
 
-
-# def test_read_queue():
-#     hero_client = hero.HeroClient()
-#     task_engine = hero_client.TaskEngine()
-#     queue = task_engine.read_queue(APP_ID, TESTABLE_QUEUE_ID)
-#     print(queue)
-#     assert queue['id'] == TESTABLE_QUEUE_ID
-
-# def test_add_and_delete_queue():
-#     hero_client = hero.HeroClient()
-#     task_engine = hero_client.TaskEngine()
-
-#     # add a queue
-#     queue_attributes = {
-#         'name': 'example_queue',
-#         'description': 'example_description'
-#     }
-#     queue = task_engine.add_queue(APP_ID, queue_attributes)
-#     tmp_queue_id = queue['id']
-#     assert queue['name'] == 'example_queue'
-
-#     # now delete the same queue
-#     queue = task_engine.delete_queue(APP_ID, tmp_queue_id)
-#     tmp_queue_id = None
-#     assert queue['GSI1SK'] == 'METATYPE#Queue|deleted'
+    # now delete the same queue
+    queue = task_engine.delete_queue(tmp_queue_id)
+    assert queue["GSI1SK"] == "METATYPE#Queue|deleted"
