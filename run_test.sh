114837c259f7834c9c13a228acb7967f684a5ae3
Sameer Shaik <sameer.shaik@nrel.gov>
2026-04-02 12:09:20 -0600

    include tip for local data repo api testing & running single test within a test suite

--- run_test.sh
+++ run_test.sh
@@ -7,6 +7,8 @@ export HERO_PROJECT="hero-test-framework"
 # the tests, please contact the project owner.
 export HERO_CLIENT_ID="306m74pgmccj9qd1ccb876fdgk"
 export HERO_CLIENT_SECRET="u21676f1mrcqmhh5m723l1og4urjbjjg0oir37d4k2j7frpa9i0"
+# if want to test with locally deployed data repo api
+# export HERO_DATA_REPO_API_URL="http://localhost:8002/data-repo/api/v1"
 
 # Note: due to network latency, the tests may fail if the server(s) is(are) too slow to respond
 # In that case, try running the tests again
@@ -15,6 +17,8 @@ export HERO_CLIENT_SECRET="u21676f1mrcqmhh5m723l1og4urjbjjg0oir37d4k2j7frpa9i0"
 pytest test
 # Or just run a single suite/collection
 # pytest -s -v test/test_ml_model_registry.py
+# Or just run a single test from a suite/collection
+# pytest -s -v test/test_data_repo.py::TestDataRepo::test_read_dataset_files_with_pagesize
 
 # Unset the environment variables
 unset HERO_ENV
